9baa1479f5b93f4dd69cf5cc61421b7cca1c4896
Bavish7 <120493406+Bavish7@users.noreply.github.com>
2025-02-11 15:56:50 +0530

    Add files via upload

---
+++ Binary Search/peak_element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// FIND PEAK ELEMENT (LEETCODE 162)
+// USING BINARY SEARCH - TC=O(LOGN), SC=O(1)
+
+int findPeakElement(vector<int> &nums)
+{
+    int s = 0;
+    int e = nums.size() - 1;
+    int mid = s + (e - s) / 2;
+    while (s < e)
+    {
+        if (nums[mid] < nums[mid + 1])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
