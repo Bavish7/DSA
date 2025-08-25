f3880fa81126612126ac15fc9cb9b36b9dfc8891
Bavish7 <bavishtaneja82@gmail.com>
2025-08-25 21:33:33 +0530

    Add files via upload

--- Array/diagonal_traverse.cpp
+++ Array/diagonal_traverse.cpp
@@ -20,15 +20,12 @@ vector<int> findDiagonalOrder(vector<vector<int>> &matrix)
 {
     if (matrix.empty() || matrix[0].empty())
         return {};
-
     int m = matrix.size(), n = matrix[0].size();
     vector<int> result(m * n);
     int row = 0, col = 0;
-
     for (int i = 0; i < m * n; i++)
     {
         result[i] = matrix[row][col];
-
         if ((row + col) % 2 == 0)
         {
             if (col == n - 1)
@@ -54,6 +51,5 @@ vector<int> findDiagonalOrder(vector<vector<int>> &matrix)
             }
         }
     }
-
     return result;
 }
